3f22a1304406bea2df4a7d0126f214dbb87a6084
David Foster <31405412+foster999@users.noreply.github.com>
2021-02-17 17:34:44 +0000

    Add visits file generation (#4)
    * Add swabs file generation function
    
    * Add visits generation function
    
    * Add visits generation function
    
    * Rename labs to lab (singular)
    
    * Fix passing correct lab files for visits
    
    * Change shorten date format string
    
    * Rename saliva file

--- example_data/schools.py
+++ example_data/schools.py
@@ -86,11 +86,11 @@ def generate_survey_responses(file_date, records, participant_ids, school_ids):
     return survey_responses
 
 
-def generate_labs_swabs(file_date, records):
+def generate_lab_swabs(file_date, records):
     """
-    Generate labs_swabs file. Depends on survey participants file.
+    Generate lab swabs file. Depends on survey participants file.
     """
-    labs_swabs_description = (
+    lab_swabs_description = (
         lambda: {
             'Sample':_('random.custom_code', mask='SIS########', digit='#'),
             'Result': _('choice', items=["Positive", "Negative"]),
@@ -100,17 +100,35 @@ def generate_labs_swabs(file_date, records):
         }
     )
 
-    schema = Schema(schema=labs_swabs_description)
-    labs_swabs = pd.DataFrame(schema.create(iterations=records))
-    labs_swabs.to_csv(f"labs_swabs_{file_date}.csv", index=False)
-    return labs_swabs
+    schema = Schema(schema=lab_swabs_description)
+    lab_swabs = pd.DataFrame(schema.create(iterations=records))
+    lab_swabs.to_csv(f"lab_swabs_{file_date}.csv", index=False)
+    return lab_swabs
 
-  
-def generate_labs_saliva(file_date, records):
+
+def generate_lab_bloods(file_date, records):
     """
-    Generate labs_saliva file.
+    Generate lab bloods file.
     """
-    labs_saliva_description = (
+    lab_bloods_description = (
+        lambda: {
+            'specimenId': _('random.custom_code', mask='#########THR', digit='#'),
+            'specimenProcessedDate': _('datetime.formatted_datetime', fmt="%Y-%m-%dT%H:%M:%SZ", start=1800, end=1802),
+            'testResult': _('choice', items=['Positive', 'Negative'])
+        }
+    )
+
+    schema = Schema(schema=lab_bloods_description)
+    lab_bloods = pd.DataFrame(schema.create(iterations=records))
+    lab_bloods.to_csv(f"lab_bloods_{file_date}.csv", index=False)
+    return lab_bloods
+
+
+def generate_lab_saliva(file_date, records):
+    """
+    Generate lab saliva file.
+    """
+    lab_saliva_description = (
         lambda: {
             'ORDPATNAME': _('random.custom_code', mask='SIS########', digit='#'),
             'SAMPLEID': _('random.custom_code', mask='H#########', digit='#'),
@@ -118,30 +136,33 @@ def generate_labs_saliva(file_date, records):
         }
     )
 
-    schema = Schema(schema=labs_saliva_description)
-    labs_saliva = pd.DataFrame(schema.create(iterations=records))
-    labs_saliva.to_csv(f"saliva_results_{file_date}.csv", index=False)
-    return labs_saliva
+    schema = Schema(schema=lab_saliva_description)
+    lab_saliva = pd.DataFrame(schema.create(iterations=records))
+    lab_saliva.to_csv(f"lab_saliva_{file_date}.csv", index=False)
+    return lab_saliva
 
   
-def generate_labs_bloods(file_date, records):
+def generate_survey_visits(file_date, records, participant_ids, swab_barcodes, blood_barcods, saliva_barcodes):
     """
-    Generate labs_bloods file.
+    Generate survey visits file. Depends on survey participants and schools files.
     """
-    labs_bloods_description = (
+    survey_visits_description = (
         lambda: {
-            'specimenId': _('random.custom_code', mask='#########THR', digit='#'),
-            'specimenProcessedDate': _('datetime.formatted_datetime', fmt="%Y-%m-%d" + "T" + "%H:%M:%S" + "Z",
-                                       start=1800, end=1802),
-            'testResult': _('choice', items=['Positive', 'Negative'])
+            'participant_id': _('choice', items=list(participant_ids)),
+            'visit_date': _('datetime.formatted_datetime', fmt="%Y-%m-%d %H:%M:%S UTC", start=1800, end=1802),
+            'swab_Sample_barcode': _('choice', items=list(swab_barcodes) + [""]),
+            'blood_thriva_barcode': _('choice', items=list(blood_barcods) + [""]),
+            'oral_swab_barcode': _('choice', items=list(saliva_barcodes)+ [""]),
+            'last_change_datetime': _('datetime.formatted_datetime', fmt="%d/%m/%Y %H:%M:%S", start=1800, end=1802),
+            'record_created_datetime': _('datetime.formatted_datetime', fmt="%d/%m/%Y %H:%M:%S", start=1800, end=1802)
         }
     )
 
-    schema = Schema(schema=labs_bloods_description)
-    labs_bloods = pd.DataFrame(schema.create(iterations=records))
-    labs_bloods.to_csv(f"labs_bloods_{file_date}.csv", index=False)
-    return labs_bloods
-  
+    schema = Schema(schema=survey_visits_description)
+    survey_visits = pd.DataFrame(schema.create(iterations=records))
+    survey_visits.to_csv(f"survey_visits_{file_date}.csv", index=False)
+    return survey_visits
+
 
 if __name__ == "__main__":
     file_date = "18010101"
@@ -161,8 +182,16 @@ if __name__ == "__main__":
         participants["schl_urn"].unique().tolist()
     )
 
-    labs_swabs = generate_lab_swabs(file_date, 10)
-
-    labs_saliva = generate_labs_saliva(file_date, 10)
+    lab_swabs = generate_lab_swabs(file_date, 10)
+    lab_bloods = generate_lab_bloods(file_date, 10)
+    lab_saliva = generate_lab_saliva(file_date, 10)
   
-    labs_bloods = generate_labs_bloods(file_date, 10)
+
+    visits = generate_survey_visits(
+        file_date,
+        100,
+        participants["participant_id"].unique().tolist(),
+        lab_swabs["Sample"].unique().tolist(),
+        lab_bloods["specimenId"].unique().tolist(),
+        lab_saliva["ORDPATNAME"].unique().tolist(),
+        )
