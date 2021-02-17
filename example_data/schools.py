bf2c03b99f7f6f2d7a467b2f457d34f501613ab6
David Foster <foster.dev999@gmail.com>
2021-02-17 17:08:17 +0000

    Add visits generation function

--- example_data/schools.py
+++ example_data/schools.py
@@ -86,11 +86,11 @@ def generate_survey_responses(file_date, records, participant_ids, school_ids):
     return survey_responses
 
 
-def generate_lab_swabs(file_date, records):
+def generate_labs_swabs(file_date, records):
     """
-    Generate lab_swabs file. Depends on survey participants file.
+    Generate labs_swabs file. Depends on survey participants file.
     """
-    lab_swabs_description = (
+    labs_swabs_description = (
         lambda: {
             'Sample':_('random.custom_code', mask='SIS########', char='@', digit='#'),
             'Result': _('choice', items=["Positive", "Negative"]),
@@ -100,10 +100,32 @@ def generate_lab_swabs(file_date, records):
         }
     )
 
-    schema = Schema(schema=lab_swabs_description)
-    lab_swabs = pd.DataFrame(schema.create(iterations=records))
-    lab_swabs.to_csv(f"lab_swabs_{file_date}.csv", index=False)
-    return lab_swabs
+    schema = Schema(schema=labs_swabs_description)
+    labs_swabs = pd.DataFrame(schema.create(iterations=records))
+    labs_swabs.to_csv(f"labs_swabs_{file_date}.csv", index=False)
+    return labs_swabs
+
+
+def generate_survey_visits(file_date, records, participant_ids, swab_barcodes, blood_barcods, saliva_barcodes):
+    """
+    Generate survey visits file. Depends on survey participants and schools files.
+    """
+    survey_visits_description = (
+        lambda: {
+            'participant_id': _('choice', items=list(participant_ids)),
+            'visit_date': _('datetime.formatted_datetime', fmt="%Y-%m-%d %H:%M:%S UTC", start=1800, end=1802),
+            'swab_Sample_barcode': _('choice', items=list(swab_barcodes) + [""]),
+            'blood_thriva_barcode': _('choice', items=list(blood_barcods) + [""]),
+            'oral_swab_barcode': _('choice', items=list(saliva_barcodes)+ [""])),
+            'last_change_datetime': _('datetime.formatted_datetime', fmt="%d/%m/%Y %H:%M:%S", start=1800, end=1802),
+            'record_created_datetime': _('datetime.formatted_datetime', fmt="%d/%m/%Y %H:%M:%S", start=1800, end=1802)
+        }
+    )
+
+    schema = Schema(schema=survey_visits_description)
+    survey_visits = pd.DataFrame(schema.create(iterations=records))
+    survey_visits.to_csv(f"survey_visits_{file_date}.csv", index=False)
+    return survey_visits
 
 
 if __name__ == "__main__":
@@ -124,4 +146,13 @@ if __name__ == "__main__":
         participants["schl_urn"].unique().tolist()
     )
 
-    swabs = generate_lab_swabs(file_date, 40)
+    swabs = generate_labs_swabs(file_date, 40)
+
+
+    visits = generate_survey_visits(
+        file_date,
+        100,
+        participants["participant_id"].unique().tolist(),
+        swabs["Sample"].unique().tolist(),
+
+        )
